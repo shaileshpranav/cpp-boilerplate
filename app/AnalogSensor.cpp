0ed1f67782184e8c9a01305c72392a19905494db
Shailesh Pranav Rajendran <shaileshpranav.r@gmail.com>
2022-10-11 19:03:27 -0400

    Added results

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -2,21 +2,15 @@
 #include <numeric>
 #include <vector>
 
-AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
+AnalogSensor::AnalogSensor(unsigned int samples) : mSamples(samples) {}
+
+AnalogSensor::~AnalogSensor() {}
+
+int AnalogSensor::Read() {
+  std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+
+  double result = std::accumulate(readings->begin(), readings->end(), 0.0) /
+                  readings->size();
+  delete readings;
+  return result;
 }
-
-AnalogSensor::~AnalogSensor()
-{
-}
-
-int AnalogSensor::Read()
-{
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
-
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
-    return result;
-}
-
-
